842f3772dc942cdf0009828c7561676283306ca3
sashavershkova <a.m.vershkova@gmail.com>
2025-04-14 20:17:06 -0700

    fix skyline

--- skyline.py
+++ skyline.py
@@ -8,21 +8,29 @@
 
 # Output: [1,3,7]
 
-# PSEUDOCODE
-# if a number is negative: ignore it
-# if a number 
+
+def my_max(iterable):
+    max_value = 0
+    for item in iterable:
+        if item > max_value:
+            max_value = item
+    return max_value
+
 
 def skyline(building_list):
     visible = []
     for i in range(len(building_list)):
+        # if a number is negative: ignore it
         if building_list[i] < 0:
             continue
-        elif building_list[i-1] == building_list[i]:
-            break
-        
+        # if a number is equal or smaller that maximum of previous numbers we already iterated through: ignore it (bc we can't see it)
+        elif building_list[i] <= my_max(building_list[0:i]):
+            continue
+        # otherwise add to the visible list
         visible.append(building_list[i])
 
     return visible
 
 print(skyline([-1, 1, 3, 7, 7, 3]))
 print(skyline([-1, 1, 3, 11, 11, 5]))
+print(skyline([-1, 3, 4, 5, 5, 5, 2, 7, 3, 11])) # we need to get 3, 4, 5, 7, 11
