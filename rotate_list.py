6550020a7f7638f82721d26c5c4299d1e4624e0c
sashavershkova <a.m.vershkova@gmail.com>
2025-04-14 18:55:06 -0700

    add rotate list functions

--- rotate_list.py
+++ rotate_list.py
@@ -1,4 +1,23 @@
-# Add your clarifying questions here
 
 def rotate_list(list, shift_by):
-    pass
+    new_list = list.copy()
+    for i in range(len(new_list)):
+        if (i + shift_by) <= len(new_list) - 1:
+            new_index = i + shift_by
+        else:
+            new_index = (i + shift_by)%len(list)
+            
+        list[new_index] = new_list[i]
+    print(list)
+
+def rotate_list_in_place(list, shift_by):
+    border_index = shift_by%len(list)
+    list.extend(list[:border_index])
+    del list[:border_index]
+    return list
+
+# rotate_list([1, 2, 3, 4, 5],3)
+# rotate_list(["d", 2, "fine", 4, 5],10)
+
+# print(rotate_list_in_place([1, 2, 3, 4, 5],3))
+print(rotate_list_in_place(["d", 2, "fine", 4, 5], 11))
--- skyline.py
+++ skyline.py
@@ -1,4 +1,23 @@
 # Add your clarifying questions here
+# Imagine a skyline of buildings and you were standing 
+# in front of them at ground level 0. 
+# How many of these buildings could you see?
+
+# Given a list [-1, 1, 3, 7, 7, 3] 
+# determine which values could be "seen."
+
+# Output: [1,3,7]
+
 
 def skyline(building_list):
-    pass
+    visible = []
+    for i in range(len(building_list)-1):
+        if building_list[i] < 0:
+            continue
+)
+        visible.append(building_list[i])
+
+    print(visible)
+
+skyline([-1, 1, 3, 7, 7, 3])
+skyline([-1, 1, 3])
